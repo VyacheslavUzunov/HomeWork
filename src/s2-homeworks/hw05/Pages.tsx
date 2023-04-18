6c521d80de21cfc9ceaaaa3fdf780bcb72ccb4e0
Vuacheslav Uzunov <slvuzunv@mail.ru>
2023-04-18 18:20:59 +0300

    com1

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -18,15 +18,15 @@ function Pages() {
             <Routes>
                 {/*роутинг будут писать студенты*/}
                 {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
-                <Route path={'/'} element={<Navigate to={'/pre-junior'} />}/>
+                <Route path={'/'} element={<PreJunior/>}/>
 
                 {/*роуты для /pre-junior, /junior, /junior-plus*/}
-                <Route path={'/pre-junior'} element={<PreJunior/>} />
-                <Route path={'/junior'} element={<Junior/>} />
-                <Route path={'/junior-plus'} element={<JuniorPlus/>}/>
+                <Route path={'pre-junior'} element={<PreJunior/>} />
+                <Route path={'junior'} element={<Junior/>} />
+                <Route path={'junior-plus'} element={<JuniorPlus/>}/>
 
                 {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
-                <Route path={'/*'} element={<Error404/>}/>
+                <Route path={'*'} element={<Error404/>}/>
             </Routes>
         </div>
     )
